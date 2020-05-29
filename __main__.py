9982bfbc039c7f97b14ad46f093f73dec6e5aaf3
Vitor <vitor.silverio.rodrigues@gmail.com>
2020-05-29 17:36:11 -0300

    fixes

--- __main__.py
+++ __main__.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 def browiki_search(query):
+    print(query)
     session = requests.Session()
     session.get('https://browiki.org/wiki/')
 
@@ -33,12 +34,13 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     channel = message.channel
-    if browiki_cmd.matches(message):
-        results = browiki_search(browiki_cmd.sub(r'$1',message))
+    message_content = message.content
+    if browiki_cmd.match(message_content):
+        results = browiki_search(browiki_cmd.sub(r'\1',message_content))
         if len(results) > 0:
-            channel.send('٩(˘◡˘)۶ Encontrei a(s) seguinte(s) página(s):\n%s' % ('\n'.join(results)))
+            await channel.send('٩(˘◡˘)۶ Encontrei a(s) seguinte(s) página(s):\n%s' % ('\n'.join(results)))
         else:
-            channel.send('(─‿‿─) Não encontrei nada na Browiki sobre o assunto')
+            await channel.send('(─‿‿─) Não encontrei nada na Browiki sobre o assunto')
 
 
 client.run(TOKEN)
